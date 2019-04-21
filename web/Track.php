4191b896911ee25fec2846b6402e36dfbf6b1943
Zakhar Telyatnikov <tz@za-ek.ru>
2019-04-21 19:03:08 +0300

    support partial download. limit bandwidth from php

--- web/Track.php
+++ web/Track.php
@@ -14,12 +14,30 @@ $stmt->bindValue(':id', $this->getAction()->getRequest()->get('id')['id'], SQLIT
 $result = $stmt->execute();
 
 if($result = $result->fetchArray(SQLITE3_NUM)) {
+    $bytesRange = [0];
+    if (!empty($_SERVER['HTTP_RANGE'])) {
+        $bytesRange = explode('-', substr($_SERVER['HTTP_RANGE'], strpos($_SERVER['HTTP_RANGE'], '=') + 1));
+    }
     $size = filesize($result[0]);
+    if(empty($bytesRange[1])) {
+        $bytesRange[1] = $size - 1;
+    }
+    $maxPerRequest = 1024 * 128;
+    if ($bytesRange[1] - $bytesRange[0] > $maxPerRequest) {
+        $bytesRange[1] = $bytesRange[0] + $maxPerRequest;
+    }
+    $length = $bytesRange[1] - $bytesRange[0];
+
+    if($length === 0) {
+        die(header('HTTP/1.1 416 Requested Range Not Satisfiable'));
+    }
+
     header('HTTP/1.1 206 Partial Content');
+    header('Accept-Ranges: bytes');
     header('Content-Disposition: inline;filename="'.$result[1].'.mp3"');
     header("X-Content-Duration: {$result[2]}");
-    header('Content-length: '.$size);
-    header("Content-Range: bytes 0-".($size-1)."/{$size}");
+    header('Content-length: '.$length);
+    header("Content-Range: bytes {$bytesRange[0]}-{$bytesRange[1]}/{$size}");
     header('Content-Type: audio/mpeg, audio/x-mpeg, audio/x-mpeg-3, audio/mpeg3');
     header('Cache-Control: private, must-revalidate');
     header('Last-Modified: '.gmdate(
@@ -27,7 +45,25 @@ if($result = $result->fetchArray(SQLITE3_NUM)) {
         (@filemtime($result[0]) > $result[3]) ? @filemtime($result[0]) : $result[3]
     ) . ' GMT' );
     if ($this->getAction()->getRequest()->getMethod() === 'GET') {
-        readfile($result[0]);
+        set_time_limit(0);
+        // readfile($result[0]);
+        $fp = fopen($result[0], 'rb');
+        fseek($fp, $bytesRange[0], SEEK_SET);
+        if (ob_get_level() == 0) ob_start();
+        $printed = 0;
+        while( !feof($fp) && $printed < $length){
+            $out = min(128, $bytesRange[1] - $printed);
+            if($out < 1) {
+                error_log('$printed: ' . $printed);
+                error_log('$length: ' . $length);
+                error_log('$size: ' . $size);
+                die();
+            }
+            echo fread($fp, $out);
+            $printed += $out;
+            flush();
+            usleep(100);
+        }
     }
     die();
 } else {
